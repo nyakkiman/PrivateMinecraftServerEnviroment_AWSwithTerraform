4475a9ad43a4d14d19e6f7b0ebcba7829a409fae
nyakkiman <nyakkiman@gmail.com>
2020-10-30 19:37:00 +0900

    create prd setting, update README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# TerraformTraining-BasicNetworkEnviroment
-Learn terraform through basic network enviroment(VPC, public_private subnet, internet gaweway, nat gateway, route table) building.
+# Simple Minecraft Personal Server Enviroment
+
+## Setup infrastructure
+
+1. Change directory to env/prd or env/stg.
+1. Edit main.tf(as proprietly).
+1. Copy ssh public key(e.g id_rsa.pub) to prd or stg folder.
+1. Execute terraform commands
+1. `terraform init`
+1. `terraform apply`
+
+## Setup minecraft server
+
+1. Connect to elastic ip with private key `"ssh -i path/to/private/key ec2-user@elasticip"`
+1. After login EC2, get to be superuser `sudo su`
+1. `yum update -y`
+1. `yum -y install java-1.8.0-openjdk-devel.x86_64 `
+1. `wget https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar`
+1. `java -Xms1G -Xmx1G -jar minecraft_server.1.16.3.jar --nogui`
+1. `vi eura.txt` edit false to true.
+1. create boot script (`vi start.sh`) and change permission to executable
+
+```
+#!/bin/sh
+cd "$(dirname "$0")"
+exec java -Xms1G -Xmx1G -jar server.jar --nogui
+```
--- env/prd/main.tf
+++ env/prd/main.tf
@@ -13,31 +13,105 @@ terraform {
   }
 }
 
+#################################################
+# Network
+#################################################
 module "vpc" {
   source       = "../../modules/vpc"
   vpc_cidr     = "10.0.0.0/16"
-  vpc_name_tag = "terraform_test_prd"
+  vpc_name_tag = "minecraft_test"
+}
+
+module "internet_gateway" {
+  source = "../../modules/igw"
+
+  attached_vpc_id           = module.vpc.vpc_id
+  internet_gateway_name_tag = "minecraft_env_test_igw"
 }
 
 module "subnet" {
   source = "../../modules/subnet"
+
   # public subnet
   public_subnet_vpc_id      = module.vpc.vpc_id
   public_subnet_vpc_cidr    = module.vpc.vpc_cidr_block
   public_subnet_cidr_range  = 8
-  public_subnet_name_prefix = "terraform_test_public_product_"
-  # Declare as many public subnets as you want
+  public_subnet_name_prefix = "minecraft_test_public_prd_"
+  # Specify AZ and declare as many public subnets as you want
   public_subnet_numbers = {
     "ap-northeast-1a" = 0
   }
+}
 
-  # private subnet
-  private_subnet_vpc_id      = module.vpc.vpc_id
-  private_subnet_vpc_cidr    = module.vpc.vpc_cidr_block
-  private_subnet_cidr_range  = 8
-  private_subnet_name_prefix = "terraform_test_private_product_"
-  # Declare as many private subnets as you want
-  private_subnet_numbers = {
-    "ap-northeast-1a" = 0
+#### routetables
+# internet gateway
+module "route_table_igw" {
+  source = "../../modules/rtable_igw"
+
+  # routetable subnet to igw
+  vpcid                         = module.vpc.vpc_id
+  targetid_to_igw               = module.internet_gateway.igw_id
+  routing_dest_cidr_from_to_igw = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "to_igw" {
+  # routetable subnet to igw
+  for_each = module.subnet.public_subnet_numbers
+
+  subnet_id      = module.subnet.public_subnet_ids[each.value]
+  route_table_id = module.route_table_igw.routetable_id_egress
+}
+
+#################################################
+# Application
+#################################################
+#### EC2
+# security group
+module "ec2_sg" {
+  source = "../../modules/sg"
+
+  sg_config = {
+    name                  = "ec2-sg"
+    vpc_id                = module.vpc.vpc_id
+    ingress_protocol_port = [{ protocol = "icmp", port = -1 }, { protocol = "tcp", port = 22 }, { protocol = "tcp", port = 25565 }]
+    ingress_cidr_blocks   = ["0.0.0.0/0"]
   }
+}
+
+# ami
+data "aws_ssm_parameter" "amazon_linux_2" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+# instance
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ssm_parameter.amazon_linux_2.value
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [module.ec2_sg.security_group_id]
+  key_name               = aws_key_pair.ec2_keypair.id
+  subnet_id              = module.subnet.public_subnet_ids[0]
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "minecraft test"
+  }
+}
+
+# key pair
+resource "aws_key_pair" "ec2_keypair" {
+  key_name = "minecraft_test_key"
+
+  # change as appropriate
+  public_key = file("./id_rsa.pub")
+}
+
+# elastic ip
+resource "aws_eip" "ec2_eip" {
+  instance   = aws_instance.ec2_instance.id
+  vpc        = true
+  depends_on = [module.internet_gateway]
 }
